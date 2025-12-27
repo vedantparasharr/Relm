c8a54b66e45c5e98c6b23bfe78bff2df31a2abf1
vedantparasharr <iemvedant@gmail.com>
2025-12-27 10:45:27 +0530

    Use auth and routes for auth

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -117,6 +117,85 @@ const handleGuest = async (req, res) => {
   res.redirect("/home");
 };
 
+const handleSignin = async (req, res) => {
+  const { email, password, remember } = req.body;
+
+  const user = await userModel.findOne({ email: email });
+  if (!user) return res.status(400).send("Invalid email or password");
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid)
+    return res.status(400).send("Invalid email or password");
+
+  if (!user.verified) {
+    sendOTPEmail(user).catch(console.error);
+    return res.render("verify", { user: user._id });
+  }
+
+  let token;
+  if (remember) {
+    token = jwt.sign(
+      { email: email, userId: user._id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "30d",
+      }
+    );
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+    });
+  } else {
+    token = jwt.sign(
+      { email: email, userId: user._id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+    });
+  }
+  res.redirect("/profile");
+};
+
+const handleForget = async (req, res) => {
+  const { email } = req.body;
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    return res.status(400).send("No account found with this email");
+  }
+  sendOTPEmail(user).catch(console.error);
+  user.otpPurpose = "reset_password";
+  user.otpHash = undefined;
+  user.otpExpires = undefined;
+  await user.save();
+  res.render("verify", { user: user._id });
+};
+
+const handleReset = async (req, res) => {
+  const user = await userModel.findById(req.params.user);
+  const { password, confirmPassword } = req.body;
+
+  if (!user) return res.status(400).send("User does not exist.");
+  if (password !== confirmPassword)
+    return res.status(400).send("Passwords does not match");
+
+  user.password = await bcrypt.hash(password, 10);
+  user.otpPurpose = undefined;
+  user.otpHash = undefined;
+  user.otpExpires = undefined;
+  await user.save();
+
+  return res.redirect("/auth/signin");
+};
+
 module.exports = {
   renderSignup,
   renderSignin,
@@ -126,4 +205,7 @@ module.exports = {
   handleSignup,
   handleVerifyEmail,
   handleGuest,
+  handleSignin,
+  handleForget,
+  handleReset,
 };
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-// mongoose.connect(process.env.MONGO_URI);
-mongoose.connect("mongodb://localhost:27017/blogapp");
+mongoose.connect(process.env.MONGO_URI);
+// mongoose.connect("mongodb://localhost:27017/blogapp");
 
 const userSchema = mongoose.Schema(
   {

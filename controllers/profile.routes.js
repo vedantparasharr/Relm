69659ba43636fcfcf49c14013b86617bcd46c0fc
vedantparasharr <iemvedant@gmail.com>
2025-12-27 23:28:45 +0530

    Setup render routes and controllers too

--- controllers/profile.routes.js
+++ controllers/profile.routes.js
@@ -44,3 +44,72 @@ const renderSettings = async (req, res) => {
   res.render("settings", { user, dayjs });
 };
 
+const handleEdit = async (req, res) => {
+  const { name, username, bio } = req.body;
+  const user = await userModel.findById(req.user.userId);
+  const existingUser = await userModel.findOne({
+    username: username.trim().toLowerCase(),
+    _id: { $ne: req.user.userId },
+  });
+
+  if (existingUser) {
+    return res.status(400).send("Username already taken");
+  }
+
+  if (!user) return res.status(404).send("User not found");
+  const imageUrl = req.file ? await uploadToSupabase(req.file) : undefined;
+
+  user.name = name?.trim();
+  user.username = username.trim().toLowerCase();
+  user.bio = bio?.trim();
+  if (req.file) user.image = imageUrl;
+  await user.save();
+
+  res.redirect("/profile");
+};
+
+const handleSettings = async (req, res) => {
+  const {
+    name,
+    username,
+    dateOfBirth,
+    website,
+    bio,
+    currentPassword,
+    newPassword,
+    confirmPassword,
+  } = req.body;
+
+  const user = await userModel.findById(req.user.userId);
+  if (!user) return res.status(404).send("User not found");
+
+  const isUserChangingPassword =
+    currentPassword || newPassword || confirmPassword;
+
+  if (isUserChangingPassword) {
+    const isPasswordValid = await bcrypt.compare(
+      currentPassword,
+      user.password
+    );
+    if (!isPasswordValid)
+      return res.status(400).send("Current password is incorrect");
+
+    if (newPassword !== confirmPassword)
+      return res.status(400).send("New passwords do not match");
+
+    user.password = await bcrypt.hash(newPassword, 10);
+  }
+
+  const imageUrl = req.file ? await uploadToSupabase(req.file) : undefined;
+
+  user.name = name;
+  user.username = username.trim().toLowerCase();
+  user.dateOfBirth = dateOfBirth;
+  user.website = website;
+  user.bio = bio;
+  if (req.file) {
+    user.image = imageUrl;
+  }
+  await user.save();
+  res.redirect("/profile");
+};

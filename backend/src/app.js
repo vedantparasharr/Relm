9c8f13375dbac2667dca384cf8dd5b07167516e3
vedantparasharr <iemvedant@gmail.com>
2026-02-16 21:01:16 +0530

    Fix route on backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,7 +27,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://relm-three.vercel.app"],
     credentials: true,
   }),
 );

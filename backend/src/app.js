9b1a2eaea5f9d037eb0e3e9d3c0d793a85ef71c1
vedantparasharr <iemvedant@gmail.com>
2026-02-16 21:13:53 +0530

    fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,7 +27,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://relm-zzse.onrender.com/"],
+    origin: ["http://localhost:5173", "https://relm-three.vercel.app"],
     credentials: true,
   }),
 );

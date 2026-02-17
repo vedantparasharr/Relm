2dc2cf4bad096681330e68cc7896872bb5894589
vedantparasharr <iemvedant@gmail.com>
2026-02-17 22:11:28 +0530

    Fix "/" Route.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,51 @@
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
 
 import Signin from "./pages/Signin";
 import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
 import Home from "./pages/Home";
 import Post from "./pages/Post";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
+const isAuthenticated = !!localStorage.getItem("token");
+
   return (
     <Routes>
-      <Route path="/signin" element={<Signin />}></Route>
-      <Route path="/profile" element={<Profile />}></Route>
-      <Route path="/profile/settings" element={<Settings />}></Route>
-      <Route path="/home" element={<Home />}></Route>
-      <Route path="/post/:id" element={<Post />}></Route>
+      <Route
+        path="/"
+        element={
+          isAuthenticated
+            ? <Navigate to="/home" replace />
+            : <Navigate to="/signin" replace />
+        }
+      />
+
+      <Route path="/signin" element={<Signin />} />
+      <Route
+        path="/home"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route path="/profile/settings" element={<Settings />} />
+      <Route path="/post/:id" element={<Post />} />
     </Routes>
   );
 };
+
 
 export default App;
---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  return isAuthenticated
+    ? children
+    : <Navigate to="/signin" replace />;
+};
+
+export default ProtectedRoute;
